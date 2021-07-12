c4e6f7fb1db73043ca662fccfcb54e000e206992
Mac <mac@makkunoMacBook-Pro.local>
2021-07-12 20:39:29 +0900

    if_else

---
+++ if_else.js
@@ -0,0 +1,32 @@
+
+ball = 125
+
+if(ball <=100 && ball >=95 ){
+    console.log("A+");
+}
+else if(ball <= 94 && ball >=90){
+    console.log("A");
+}
+else if(ball <= 89 && ball >=85){
+    console.log("B+");
+} 
+else if(ball <= 84 && ball >= 80){
+    console.log("B");
+}
+else if(ball <=79 && ball >= 75){
+    console.log("c+");
+}
+else if(ball <=74 && ball >= 70){
+    console.log("C");
+}
+else if(ball <= 69 && ball >= 65){
+    console.log("D+");
+}
+else if(ball<=64 && ball>=60){
+    console.log("D");
+}else if(ball<=59 && ball>=0){
+    console.log("FAIL");
+}
+else{
+    console.log("Faqat 100 BALL lik sistemada kiritaolsiz");
+}
